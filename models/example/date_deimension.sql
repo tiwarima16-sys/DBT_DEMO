ef30eeaadfbf956b7b09ed77c96d8aacd4dabbb8
Manish Tiwari <tiwari.ma16@gmail.com>
2026-08-04 12:03:10 +0000

    daily_weather_model_created

--- models/example/date_deimension.sql
+++ models/example/date_deimension.sql
@@ -3,19 +3,12 @@ WITH CTE AS (
         HOUR(TRY_TO_TIMESTAMP(STARTED_AT)) as HOUR_STARTED_AT,
         DAYNAME(TRY_TO_TIMESTAMP(STARTED_AT)) as DAY_STARTED_AT,
         
-        CASE
-            WHEN DAYNAME(TRY_TO_TIMESTAMP(STARTED_AT)) in ('Sat', 'Sun') THEN 'WEEKEND'
-            ELSE 'BUSINESSDAY'
-        END as DATETYPE,
+         {{get_datetype('STARTED_AT')}} as DATETYPE,
 
         MONTH(TRY_TO_TIMESTAMP(STARTED_AT)) as MONTH_STARTED_AT,
 
-        CASE 
-            WHEN MONTH(TRY_TO_TIMESTAMP(STARTED_AT)) in (12,1,2) THEN 'WINTER'
-            WHEN MONTH(TRY_TO_TIMESTAMP(STARTED_AT)) in (3,4,5) THEN 'SPRING'
-            WHEN MONTH(TRY_TO_TIMESTAMP(STARTED_AT)) in (6,7,8) THEN 'SUMMER'
-            ELSE 'AUTUMN'
-        END AS STATION_OF_YEAR
+        {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
+
 
     from {{ source('demo', 'bike') }}
 )
